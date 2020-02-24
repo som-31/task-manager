21c7a7742140c073bf74483c968d5a5bb06ef66b
Somnath Jadhav <somnath.jadhav@lapr075.successivetech.com>
2020-02-24 18:54:54 +0530

    added separate router files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const { ObjectID } = require('mongodb');
 require('./db/mongoose');
-const User = require('./models/user');
-const Task = require('./models/task');
-
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task');
 
 const app = express();
 
@@ -13,89 +12,20 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 
-app.post('/users', async (req, res) => {
-    const user = new User(req.body);
-    
-    try{
-        await user.save();
-         res.status(201).send(user); 
-    }catch (e) {
-        res.status(400).send(e);
-    }
+//include Routers
+app.use(userRouter)
+app.use(taskRouter)
+const bcrypt = require('bcryptjs')
 
-});
-
-app.get('/users', async (req, res) => {
-    
-    try {
-       const users = await User.find({})
-       res.send(users);
-    }catch (e){
-       res.status(500).send();
-    }
-});
-
-app.get('/users/:id', async (req, res) => {
-
-    try{
-        const _id = req.params.id;
-        const user =  await User.findById(_id)
-        if(!user){
-            return res.status(404).send();
-       }
-
-       res.send(user);
-    }catch (e) {
-        res.status(500).send();
-    }
-});
-
-app.patch('/users/:id', async (req, res) => {
-     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new : true, runValidators : true})
-        if(!user){
-            return res.status(404).send();
-        }
- 
-        res.send(user);
-
-     } catch (error) {
-      res.status(500).send();   
-     }
-})
-
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body);
-    try {
-         await task.save();
-         res.status(201).send(task);
-    }catch (e) {
-        res.status(400).send(e);
-    }
-});
-
-app.get('/tasks', async (req, res) => {
-    try {     
-    const tasks = await Task.find({});
-    res.send(tasks)
-    } catch (error) {
-        res.status(500).send();
-    }
-});
-
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id ;
-    try {
-        const task = await Task.findById(_id);
-        if(!task){
-            return res.status(404).send();
-        }
-
-        res.send(task);
-    } catch (error) {
-        res.status(500).send();
-    }
-});
+const sample = async () => {
+    const password = 'Som_31@idfc';
+    const hashedPassword = await bcrypt.hash(password, 8)
+    console.log(password)
+    console.log(hashedPassword)
+    const isMatch = await bcrypt.compare('Som_31@ifc', hashedPassword);
+    console.log(isMatch);
+}
+sample();
 
 app.listen(port, () => {
     console.log('App is up and running on port '+port);
