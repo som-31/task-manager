3b9490fdb07fbce328292707a76c5d25a6820533
Somnath Jadhav <somnath.jadhav@lapr075.successivetech.com>
2020-03-11 19:01:24 +0530

    spark added for file cropping

--- src/index.js
+++ src/index.js
@@ -11,27 +11,6 @@ const port = process.env.PORT || 3000;
 //to parse incoming json data to object
 app.use(express.json());
 
-
-const multer = require('multer');
-const upload = multer({
-    dest : 'images',
-    limits : {
-        fileSize : 1000000
-    },
-    fileFilter(req, file, cb){
-        if(!file.originalname.match(/\.(doc|docx)$/)){
-            return cb(new Error('Please upload a word document'))
-        }
-
-        cb(undefined, true);
-    }
-})
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send();
-})
-
-
 //include Routers
 app.use(userRouter)
 app.use(taskRouter)
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 
@@ -8,7 +9,16 @@ const router = new express.Router();
 
 //configure multer
 const upload = multer({
-    dest : 'avatars'
+    limits : {
+        fileSize : 1000000
+    },
+    fileFilter(req, file, cb){
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+            return cb(new Error('Please provide an image'));
+        }
+
+       cb(undefined, true)
+    }
 })
 
 
@@ -94,8 +104,37 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 })
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({height : 250, width : 250}).png().toBuffer();
+    req.user.avatar = buffer
+    await req.user.save();
     res.send();
+}, (error, req, res, next)=> {
+    res.status(400).send({
+        error : error.message
+    })
+})
+
+router.delete('/users/me/avatar', auth , async (req, res) => {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send()
+});
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if(!user || !user.avatar){
+           throw new Error();
+        }
+
+        res.set('Content-Type', 'image/png');
+        res.send(user.avatar);
+
+    } catch (error) {
+        res.status(404).send()
+    }
 })
 
 module.exports = router
